56f3132230b3810acd56d7bb821320a911875cfc
Jeremy Kennard <13806019+iamvalion@users.noreply.github.com>
2022-09-07 04:30:16 -0600

    Add HandleLocations/ and HandleTriggers/

---
+++ HandleLocations/src/java/me/valion/bukkit/handle/locations/HandleLocations.java
@@ -0,0 +1,19 @@
+package me.valion.bukkit.handle.locations;
+
+import me.valion.bukkit.handle.core.HandleCore;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HandleLocations extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		// Adds data files to be managed by Handle's FilingCabinet
+		HandleCore.getFilingCabinet().addDataFile("locations/settings.yml");
+		HandleCore.getFilingCabinet().addDataFile("locations/storage.yml");
+		
+		// Registers commands
+		getCommand("location").setExecutor(new LocationCommandExecutor());
+		getCommand("locations").setExecutor(new LocationsCommandExecutor());
+	}
+}
